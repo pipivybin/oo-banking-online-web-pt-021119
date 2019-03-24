bf63f045662a71618add577340821c129d6ff23c
Ming Duan <11203duan@gmail.com>
2019-03-24 22:19:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,8 +26,9 @@ def execute_transaction
 end
 
 def reverse_transfer
-
-
+  if self.status == "complete"
+    self.sender.balance += self.amount
+    self.receiver.balance -= self.amount
 end
 
 end

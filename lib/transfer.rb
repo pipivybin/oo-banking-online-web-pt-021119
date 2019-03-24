3048a680baaf188f84a52f8d35d8dbe23ce62a70
Ming Duan <11203duan@gmail.com>
2019-03-24 22:12:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -5,7 +5,8 @@ attr_accessor :sender, :receiver, :amount, :status
 def initialize(sender, receiver, amount)
   @sender = sender
   @receiver = receiver
-
+  @amount = amount
+  @status = "pending"
 end
 
 
